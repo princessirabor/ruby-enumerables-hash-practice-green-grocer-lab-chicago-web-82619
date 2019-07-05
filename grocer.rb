1f87e956a7af06b2aced88ce8f3b827df5a15057
Princess Irabor <irabor.princess1@gmail.com>
2019-07-05 05:53:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,17 @@
 def consolidate_cart(cart)
   # code here
+  keyarray= []
+  emptyHash = {}
+    cart.each do |eachcart|
+    eachcart.reduce({}) do |memo, (key, value)|
+    keyarray.push(key)
+    emptyHash[key] = value
+    end
+  end
+  keyarray.each do |eachkey|
+    emptyHash[eachkey][:count] = keyarray.count(eachkey)
+  end
+  emptyHash
 end
 
 def apply_coupons(cart, coupons)
@@ -13,3 +25,13 @@ end
 def checkout(cart, coupons)
   # code here
 end
+
+
+
+
+
+
+
+
+
+
